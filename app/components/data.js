258834036456756d3e9b8b5e1c684f51f19ac0a5
farouk <noblefarz7@gmail.com>
2025-06-07 23:22:20 +0100

    firstUpdate

---
+++ app/components/data.js
@@ -0,0 +1,62 @@
+import eventree from "../../public/eventree.png"
+import cosmos from "../../public/cosmos.png"
+
+
+export const portData = [
+    {
+        id:1,
+        name:"Eventree Website",
+        description:"Eventree is an event management system that focus on helping users get the best of their event by providing them with suitable vendors that carter for their events.",
+        img: eventree,
+        link:"https://eventreelandingpage.netlify.app/"
+    },
+
+     {
+    id:2,
+    link:"https://cosmos-remits.vercel.app/",
+    description:"Our Web App helps users to send money quickly and seamlessly to their love ones across the globe with little to no charge, read more on the website",
+    name:"CosmosRemit Website",
+    img: cosmos
+  },
+
+]
+
+
+//   {
+//     id:1,
+//     link:"https://teevill.netlify.app/",
+//     title:"Simple Auth Flow",
+//     img: teevil
+//   },
+//  
+//   {
+//     id:3,
+//     link:"https://divvicson.netlify.app/",
+//     title:"Divvicson Academy Website",
+//     img: div
+//   },
+//   {
+//     id:4,
+//     link:"https://leadcapital.netlify.app/",
+//     title:"Lead Capital Logistic Website",
+//     img: leadCapital
+//   },
+//   // {
+//   //   id:5,
+//   //   link:"https://howbodi.io/",
+//   //   title:"Howbodi Mental HealthCare",
+//   //   img: howbodi
+//   // },
+//   {
+//     id:6,
+//     link:"https://task-naija.netlify.app",
+//     title:"TaskNaija",
+//     img: taskNaij
+//   },
+//   {
+//     id:7,
+//     link:"https://eventreelandingpage.netlify.app/",
+//     title:"Eventree",
+//     img: eventree
+//   },
+// ]
--- app/page.js
+++ app/page.js
@@ -5,6 +5,9 @@ import { BsDashLg } from "react-icons/bs";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithubSquare } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
+import { portData } from "./components/data";
+import Image from "next/image";
+import Link from "next/link";
 
 const sections = ["about", "experience", "projects"];
 
@@ -32,10 +35,10 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex h-full justify-center bg-[#010916] px-[120px] py-[50px] gap-[70px] ">
+    <div className="flex flex-col px-[20px]    md:flex-row h-full justify-center bg-[#010916] md:px-[120px] py-[40px] gap-[20px] ">
       <CursorSpotlight />
       {/* Sidebar */}
-      <aside className="flex-2 p-6 sticky top-30 self-start h-full text-white">
+      <aside className="flex-2 p-6 self-start h-full text-white md:sticky md:top-10">
         <nav className="space-y-4 flex-col ">
           <h1 className="text-[50px] font-extrabold">
             Oyedeji Opeoluwa Farouk
@@ -65,28 +68,28 @@ export default function Home() {
           ))}
         </nav>
         <div className="flex gap-5 mt-[50px]">
-          <FaLinkedin size={30} className="cursor-pointer"/>
-          <FaGithubSquare size={30} className="cursor-pointer"/>
-          <FaSquareXTwitter size={30} className="cursor-pointer"/>
+          <FaLinkedin size={30} className="cursor-pointer" />
+          <FaGithubSquare size={30} className="cursor-pointer" />
+          <FaSquareXTwitter size={30} className="cursor-pointer" />
         </div>
       </aside>
 
       {/* Main Content */}
-      <main className="flex-3 overflow-y-scroll hide-scrollbar p-6 mt-30 space-y-32">
+      <main className="flex-2 overflow-y-scroll hide-scrollbar p-6 mt-30 space-y-32">
         <section id="about" className="mb-[100px] scroll-mt-20">
           <h2 className="text-3xl font-semibold">About</h2>
           <p className="py-5 text-[20px]">
             Good Morning, welcome to my page. My name is Oyedeji Opeoluwa
-            Farouk, and I am a Frontend Web Developer based
-            in Lagos State, Nigeria. I create responsive and scalable websites
-            for companies and businesses. A graduate of Microbiology from Yaba
-            College of Technology, Fuelled by a passion for developing user
-            friendly and scalable products. I have a deep to excel and
-            continuously improve in my work.I am passionate, posess good
-            communication skill which makes me good with working with team
-            members. <br/><br/>I am also a logical and critical thinker that thinks
-            outside the box to get problems solved. I love combining the worlds
-            of logic and creative design to make eye-catching, accessible, and
+            Farouk, and I am a Frontend Web Developer based in Lagos State,
+            Nigeria. I create responsive and scalable websites for companies and
+            businesses. A graduate of Microbiology from Yaba College of
+            Technology, Fuelled by a passion for developing user friendly and
+            scalable products. I have a deep to excel and continuously improve
+            in my work.I am passionate, posess good communication skill which
+            makes me good with working with team members. <br />
+            <br />I am also a logical and critical thinker that thinks outside
+            the box to get problems solved. I love combining the worlds of logic
+            and creative design to make eye-catching, accessible, and
             user-friendly website and applications.When I am not on my laptop
             writing codes, I enjoy my time playing video games and listening to
             music.
@@ -96,34 +99,65 @@ export default function Home() {
         <section id="experience" className="mb-[100px] scroll-mt-20">
           <h2 className="text-3xl font-semibold">Experience</h2>
           {/* <p>Experience content goes here...</p> */}
-          <div className="flex   mt-[20px] text-[20px]">
+          <div className="flex gap-2 mt-[20px] text-[20px]">
             <p className="flex-1">Jan 2023 - May 2025</p>
             <p className="flex-2">
-              Contributed to various projects, including the developement of admin and users portals for 
-              different organization,I also collaborated with a team of five to design scalable applications for clients.
-              <br/>
-              I also created a website(E-Learning) website for a client, I was rsponsible for the development of the UI designs
-              as well as making it responsive accross all screen size.
-              Created the Admin and the Student portal for the users and the company respectively. 
+              Contributed to various projects, including the developement of
+              admin and users portals for different organization,I also
+              collaborated with a team of five to design scalable applications
+              for clients.
+              <br />I also created a website(E-Learning) website for a client, I
+              was rsponsible for the development of the UI designs as well as
+              making it responsive accross all screen size. Created the Admin
+              and the Student portal for the users and the company respectively.
             </p>
           </div>
 
-           <div className="flex   mt-[40px] text-[20px]">
+          <div className="flex gap-2 mt-[40px] text-[20px]">
             <p className="flex-1">Jan 2022 - May 2025</p>
             <p className="flex-2">
-              Contributed to various projects, including the developement of admin and users portals for 
-              different organization,I also collaborated with a team of five to design scalable applications for clients.
-              <br/><br/>
-              I also created a website(E-Learning) website for a client, I was rsponsible for the development of the UI designs
-              as well as making it responsive accross all screen size.
-              Created the Admin and the Student portal for the users and the company respectively. 
+              Contributed to various projects, including the developement of
+              admin and users portals for different organization,I also
+              collaborated with a team of five to design scalable applications
+              for clients.
+              <br />
+              <br />I also created a website(E-Learning) website for a client, I
+              was rsponsible for the development of the UI designs as well as
+              making it responsive accross all screen size. Created the Admin
+              and the Student portal for the users and the company respectively.
             </p>
           </div>
         </section>
 
         <section id="projects" className="min-h-screen scroll-mt-20">
           <h2 className="text-3xl font-semibold">Projects</h2>
-          <p>Projects content goes here...</p>
+          <p>
+            Below are some of the works I have done as a team and individually.
+            You are welcome to go through them.
+          </p>
+          <div className="mt-[20px]">
+            
+            {portData?.map((data) => (
+              <Link key={data?.id} href={data?.link}>
+              <div
+                // key={}
+                className="flex p-[20px] mb-5 rounded-[10px] gap-10 hover:bg-white/20 cursor-pointer  transition-colors ease-in duration-300"
+              >
+                <Image alt="" src={data?.img} height={200} width={200} />
+                <div>
+                  <p className=" font-bold">{data?.name}</p>
+                  <p className="text-sm py-3">
+                    {data?.description}
+                  </p>
+                  <div className="flex">
+                    <p>React</p>
+
+                  </div>
+                </div>
+              </div>
+              </Link>
+            ))}
+          </div>
         </section>
       </main>
     </div>
